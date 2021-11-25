f9ff41fd47e6e959a127cff110e2a3aae83f0b10
Oto-obong Eshiett <eshiett1995@gmail.com>
2021-11-25 11:52:53 +0100

    pushed for Victoria

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('sign-in', { title: 'Express' });
+  res.render('sign-in', { title: 'Swipe' });
 });
 
 module.exports = router;
