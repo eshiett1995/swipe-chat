8359a637cf918321aee5b77bb0102e1b79bff14b
Oto-obong Eshiett <eshiett1995@gmail.com>
2021-12-21 20:44:22 +0100

    pushed for Victoria

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const UserModel = require('./../models/user.model')
 const ArticleModel = require('./../models/article.model')
+const nodemailer = require('nodemailer');
 
 /* GET users listing. */
 router.post('/sign-up', async function(req, res, next) {
@@ -13,6 +14,30 @@ router.post('/sign-up', async function(req, res, next) {
 router.post('/login', async function(req, res, next) {
     console.log(req.body);
     let response = await UserModel.login(req.body);
+    if(response.success){
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'tellis.events@gmail.com',
+                pass: 'Tellisevents.'
+            }
+        });
+
+        const mailOptions = {
+            from: 'vikky@gmail.com',
+            to: req.body.email,
+            subject: 'This is your code',
+            text: Math.ceil(Math.random() * (99999 - 10000) + 10000).toString()
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+    }
     res.send(response);
 });
 
